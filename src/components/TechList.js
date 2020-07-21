6dce5d84c77ec819fd386c6badf2a5a1cd68bbea
joilsonLeal <joilson_rg@hotmail.com>
2020-07-21 18:50:13 -0300

    set state

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 class TechList extends Component {
   state = {
+    newTech: '',
     techs: [
       'Node.Js',
       'ReactJS',
@@ -9,13 +10,41 @@ class TechList extends Component {
     ],
   };
   
+  handleInputChange = e => {
+    this.setState({ newTech: e.target.value });
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.setState(
+      { 
+        techs: [...this.state.techs, this.state.newTech],
+        newTech: ''
+      }
+    );
+  }
+
+  handleDelete = (tech) => {
+    this.setState({ techs: this.state.techs.filter(t => t !== tech) })
+  }
+
   render() {
     return (
-      <ul>
-        <li>Node.Js</li>
-        <li>ReactJS</li>
-        <li>React Native</li>
-      </ul>
+      <form onSubmit={this.handleSubmit}>
+        <ul>
+          {this.state.techs.map(tech => 
+            <li key={tech}>
+              {tech}
+              <button onClick={() => this.handleDelete(tech)} type="button">Remove</button>
+            </li>)}
+        </ul>
+        <input 
+          type="text" 
+          onChange={this.handleInputChange} 
+          value={this.state.newTech}
+        />
+        <button type="submit">Submit</button>
+      </form>
     );
   }
 }
